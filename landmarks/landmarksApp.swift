a0098b0ca4b85cbad54a31704984e397dcb79ab7
daniel1775 <dnieto@mobohubb.com>
2025-08-20 06:11:41 -0500

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by fpena on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ landmarksTests/landmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksTests.swift
+//  landmarksTests
+//
+//  Created by fpena on 20/08/25.
+//
+
+import Testing
+@testable import landmarks
+
+struct landmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
