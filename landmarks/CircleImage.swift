3edd9459e7e998cd953c676715f611aa165587ad
daniel1775 <danilog1000@gmail.com>
2025-08-23 11:23:01 -0500

    feat: Image view added

---
+++ landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by Daniel on 23/08/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("jungle")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 300, height: 300)
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
