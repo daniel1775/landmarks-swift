97f3aa4294e89e1e0e503fbaf38d51b1d7ade20f
daniel1775 <danilog1000@gmail.com>
2025-08-25 08:33:10 -0500

    feat: landmark model added

---
+++ landmarks/models/Landmark.swift
@@ -0,0 +1,22 @@
+//
+//  Landmark.swift
+//  landmarks
+//
+//  Created by Daniel on 24/08/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+}
