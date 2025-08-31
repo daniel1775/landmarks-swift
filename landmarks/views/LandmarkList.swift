64e4a62ef5df9e71c9afc0e1e7647c59c5d0e8ea
daniel1775 <danilog1000@gmail.com>
2025-08-30 19:55:41 -0500

    feat: List added

--- landmarks/views/LandmarkList.swift
+++ landmarks/views/LandmarkList.swift
@@ -11,6 +11,8 @@ struct LandmarkList: View {
     var body: some View {
         List {
             LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
         }
     }
 }
