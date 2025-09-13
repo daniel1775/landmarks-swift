48d70f1e5337953e5466eb5a2fcbbf9851cbbcf9
daniel1775 <danilog1000@gmail.com>
2025-09-13 10:46:10 -0500

    feat: FavoriteButton added

---
+++ landmarks/views/Helper/FavoriteButton.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isFavorite: Bool;
+    
+    var body: some View {
+        Button(action: { isFavorite.toggle() }) {
+            Label("Toggle Favorite", systemImage: isFavorite ? "star.fill" : "star")
+                    .labelStyle(.iconOnly)
+                    .foregroundStyle(isFavorite ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isFavorite: Binding.constant(true))
+}
