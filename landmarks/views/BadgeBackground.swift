66dd036e59c1305139fa8d9293b837d0f11935c4
daniel1775 <danilog1000@gmail.com>
2025-09-23 18:59:15 -0500

    feat: hexagon built

---
+++ landmarks/views/BadgeBackground.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        // builder closure -> It is a kind of construct; it works as a callback on JS
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = 100.0
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95,
+                        y: height * (0.20 + HexagonParameters.adjustment)
+                    )
+                )
+                
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x,
+                            y: height * segment.line.y
+                        )
+                    )
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x,
+                            y: height * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
+                
+            }.fill(.black)
+        }
+        
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
